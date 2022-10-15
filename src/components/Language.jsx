dc06b047ac143e41ead29e5e50af1fa3d17a89d0
Hakan Bilgi <hknblg34@gmail.com>
2022-10-15 13:13:40 +0300

    image

--- src/components/Language.jsx
+++ src/components/Language.jsx
@@ -1,6 +1,16 @@
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+
 const Language = ({ name, img, options }) => {
-  // console.log(lang);
-  return <div>Language</div>;
+  console.log(name, img, options);
+  return (
+    <Container style={{ background: "peachpuff" }} className="p-4 rounded-2">
+      <Container>
+        <Image src={img} width="70%"></Image>
+        <h3 className="display-6">{name}</h3>
+      </Container>
+    </Container>
+  );
 };
 
 export default Language;
